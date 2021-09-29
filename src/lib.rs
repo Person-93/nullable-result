b363aacf244dc3c370564ec6863d27cb17dd048b
person93 <person93.person93@gmail.com>
2021-09-29 18:52:47 -0400

    feat: add the type and implement the From trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,52 @@
-#[cfg(test)]
-mod tests {}
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub enum NullableResult<T, E> {
+    Ok(T),
+    Err(E),
+    None,
+}
+
+impl<T, E> From<Result<Option<T>, E>> for NullableResult<T, E> {
+    #[inline]
+    fn from(res: Result<Option<T>, E>) -> Self {
+        match res {
+            Ok(Some(item)) => NullableResult::Ok(item),
+            Ok(None) => NullableResult::None,
+            Err(err) => NullableResult::Err(err),
+        }
+    }
+}
+
+impl<T, E> From<NullableResult<T, E>> for Result<Option<T>, E> {
+    #[inline]
+    fn from(nr: NullableResult<T, E>) -> Self {
+        match nr {
+            NullableResult::Ok(item) => Ok(Some(item)),
+            NullableResult::Err(err) => Err(err),
+            NullableResult::None => Ok(None),
+        }
+    }
+}
+
+impl<T, E> From<Option<Result<T, E>>> for NullableResult<T, E> {
+    #[inline]
+    fn from(opt: Option<Result<T, E>>) -> Self {
+        match opt {
+            None => NullableResult::None,
+            Some(Ok(item)) => NullableResult::Ok(item),
+            Some(Err(err)) => NullableResult::Err(err),
+        }
+    }
+}
+
+impl<T, E> From<NullableResult<T, E>> for Option<Result<T, E>> {
+    #[inline]
+    fn from(nr: NullableResult<T, E>) -> Self {
+        match nr {
+            NullableResult::Ok(item) => Some(Ok(item)),
+            NullableResult::Err(err) => Some(Err(err)),
+            NullableResult::None => None,
+        }
+    }
+}
---
+++ tests/basic_usage.rs
@@ -0,0 +1,65 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use nullable_result::NullableResult;
+
+mod result_and_back {
+    use super::*;
+
+    #[test]
+    fn ok() {
+        let res = Ok(Some(1));
+        let nr: NullableResult<_, u32> = NullableResult::from(res);
+        assert!(matches!(nr, NullableResult::Ok(_)));
+        let res = Result::from(nr);
+        assert_eq!(res.unwrap().unwrap(), 1);
+    }
+
+    #[test]
+    fn none() {
+        let res = Ok(None);
+        let nr: NullableResult<(), ()> = NullableResult::from(res);
+        assert!(matches!(nr, NullableResult::None));
+        let res = Result::from(nr);
+        assert!(res.unwrap().is_none());
+    }
+
+    #[test]
+    fn err() {
+        let res = Err(1);
+        let nr: NullableResult<(), _> = NullableResult::from(res);
+        assert!(matches!(nr, NullableResult::Err(_)));
+        let res = Result::from(nr);
+        assert_eq!(res.unwrap_err(), 1);
+    }
+}
+
+mod option_and_back {
+    use super::*;
+
+    #[test]
+    fn ok() {
+        let opt = Some(Ok(1));
+        let nr: NullableResult<_, ()> = NullableResult::from(opt);
+        assert!(matches!(nr, NullableResult::Ok(_)));
+        let opt: Option<Result<_, _>> = Option::from(nr);
+        assert_eq!(opt.unwrap().unwrap(), 1);
+    }
+
+    #[test]
+    fn none() {
+        let opt = None;
+        let nr: NullableResult<(), ()> = NullableResult::from(Ok(opt));
+        assert!(matches!(nr, NullableResult::None));
+        let opt: Option<Result<_, _>> = Option::from(nr);
+        assert!(opt.is_none());
+    }
+
+    #[test]
+    fn err() {
+        let opt = Some(Err(1));
+        let nr: NullableResult<(), _> = NullableResult::from(opt);
+        assert!(matches!(nr, NullableResult::Err(_)));
+        let opt: Option<Result<_, _>> = Option::from(nr);
+        assert_eq!(opt.unwrap().unwrap_err(), 1);
+    }
+}
